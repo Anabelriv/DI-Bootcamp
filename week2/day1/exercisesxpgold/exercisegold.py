58e62afd4c0f07265ad913053ec0c8461792291d
anabelriv <anabelriverav@gmail.com>
2023-07-16 14:31:04 +0300

    correcting the space in the first exercise

---
+++ week2/day1/exercisesxpgold/exercisegold.py
@@ -0,0 +1,16 @@
+# Exercise 1
+print(("Hello World\n")*4+("I love Python\n")*4)
+
+# Exercise 2
+user_input= int(input("Enter a month of the year in numbers "))
+
+if 3 >= user_input <= 5:
+    print("It's Spring")
+elif 6 >= user_input <= 8:
+    print("It's summer")
+elif 9 >= user_input <= 11:
+    print("It's Autumn")
+else:
+    print("It's winter")
+
+
