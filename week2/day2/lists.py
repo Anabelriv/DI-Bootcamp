e995fa3c2d499213e9b69400877d8655b7f3ebba
anabelriv <anabelriverav@gmail.com>
2023-07-17 12:27:11 +0300

    class practice

---
+++ week2/day2/lists.py
@@ -0,0 +1,49 @@
+# # colors=["blue","yellow","red"]
+# # first_color = colors[0]
+
+# elements=["pear", "orange", "yellow", ["apple", "pineapple"]]
+# fruit_element= elements[3][1]
+# print(fruit_element)
+
+# elements.append("Sara")
+# print(elements)
+
+# elements.insert(1,"Albert")
+# print(elements)
+# all_colors=["red","yellow"]
+# elements.insert(-1,"lola")
+# user_answer=input("give me a number and a color sep by -")
+# list_answered= user_answer.split("-")
+# all_colors.insert(int(list_answered)[0],list_answered[1])
+# print(all_colors)
+
+# all_colors[1]="pink"
+# print(all_colors)
+
+# all_colors.pop()
+# all_colors.remove("pink")
+# my_colors= all_colors[0:2]
+
+# for number in range(0,3):
+#     print("Hello:", number)
+
+# list of 15 numbers
+# if the number is odd --> the number is odd
+
+
+for num in range(0,15):
+    if num%2 ==0
+    print(f"This is an even number {num}")
+else:
+    print(f"The number is off {num}")
+
+
+for num in range(0,15):
+    if num %2 ==0 :
+        result="even"
+    else:
+        result="odd"
+
+print(f"The number {num} is {result}")
+
+
