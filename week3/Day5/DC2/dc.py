5b7198b3bb95485cf80af8518d9f69466bc732b8
anabelriv <anabelriverav@gmail.com>
2023-07-27 16:28:32 +0300

    OOP Quiz

--- week3/Day5/DC2/dc.py
+++ week3/Day5/DC2/dc.py
@@ -1,28 +1,21 @@
 # # Quizz
-# # Class: It's a blueprint used in OOP to create objects, add attributes and method that the objects will have, this helps to create objects with specific properties
-# # Instance: This is the same as an object, each one has its own attributes and it can call the methods defined in the class
-## Encapsulation: It's the containeraziation
+#       Class: It's a blueprint used in OOP to create objects, add attributes and methods that the objects will have, this helps to create objects with specific properties
 
-# 3. What is encapsulation?
-#    Encapsulation is one of the four fundamental principles of object-oriented programming (OOP). It refers to the bundling of data (attributes) and methods (functions) that operate on that data, within a single unit (class). The main idea is to hide the internal implementation details of the class from the outside world, allowing access to the data through well-defined methods (getters and setters). This concept helps achieve data abstraction and information hiding.
+#       Instance: This is the same as an object, each one has its own attributes and it can call the methods defined in the class
 
-# 4. What is abstraction?
-#    Abstraction is another fundamental principle of OOP. It is the process of simplifying complex systems by representing only the essential features and ignoring irrelevant details. In Python, abstraction can be achieved through abstract classes and interfaces. It allows developers to create a blueprint or a model for a class without implementing all the methods, thus providing a clear separation between the interface and implementation.
+#       Encapsulation: It's the containeraziation of all the attributes and methods that function on the objects, within one class. It helps to also build methods privately and to allow some changes with getters and setters.
 
-# 5. What is inheritance?
-#    Inheritance is a mechanism in object-oriented programming that allows a class (called the derived or subclass) to inherit properties and behaviors from another class (called the base or superclass). The derived class can access the attributes and methods of the base class without redefining them. It promotes code reuse and helps in creating a hierarchical relationship among classes.
+#       Abstraction: It's to simplify the complex codes building a program and to show only the essential feautures. 
 
-# 6. What is multiple inheritance?
-#    Multiple inheritance is a feature in some object-oriented programming languages, including Python, that allows a class to inherit from more than one base class. This means a derived class can access attributes and methods from multiple parent classes. However, multiple inheritance can lead to complexities and the "diamond problem" if not handled carefully.
+#       Inheritance: it's a mechanism that allows a class to inherit properties from another class (super class), this can be done withuot redifining them 
 
-# 7. What is polymorphism?
-#    Polymorphism is a concept in OOP that allows objects of different classes to be treated as objects of a common base class. It enables a single interface to represent different data types or classes. In Python, polymorphism is typically achieved through method overriding, where derived classes provide their own implementation for methods with the same name as the base class.
+#       Multiple inheritance: this mechanism allows a class to inherit from more than one base class.This is not programmer friendly and can raise complexities
 
-# 8. What is method resolution order or MRO?
-#    Method Resolution Order (MRO) is the order in which classes are searched for method resolution in a class hierarchy. In Python, when a method is called on an object, the interpreter searches for the method in the class of the object, and if not found, it searches in the parent classes in a specific order determined by the C3 linearization algorithm. The MRO ensures that the correct method is invoked when there is multiple inheritance and avoids ambiguity in method resolution.
-# #
-# #
-# #
+#       Polymorphism: this allows objects from different classes to be like objects from a common class 
+
+#       Method Resolution Order (MRO) is the order in which classes are searched for method resolution in a class hierarchy. 
+
+# 
 
 # # Part 2: Create A Deck Of Cards Class.
 # # The Deck of cards class should NOT inherit from a Card class.
