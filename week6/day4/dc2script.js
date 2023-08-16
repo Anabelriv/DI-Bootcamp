57cb50b29ca4e11725ca26bcefbe1c8cb4f27b5b
anabelriv <anabelriverav@gmail.com>
2023-08-16 15:28:06 +0300

    dc2complete

--- week6/day4/dc2script.js
+++ week6/day4/dc2script.js
@@ -8,3 +8,13 @@ for (let i = 1; i <= height; i++) {
     }
     console.log(row);
 }
+
+const maxValue = 6;
+
+for (let i = 1; i <= maxValue; i++) {
+    let row = '';
+    for (let j = 1; j <= i; j++) {
+        row += '* ';
+    }
+    console.log(row);
+}
