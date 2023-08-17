3a241e92986202c098c7216b0d24124eb400a425
anabelriv <anabelriverav@gmail.com>
2023-08-17 17:01:52 +0300

    xp

--- week6/day5/miniproject/bike_store/rent/models.py
+++ week6/day5/miniproject/bike_store/rent/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 
+
 class Address(models.Model):
     address = models.CharField(max_length=100)
     address2 = models.CharField(max_length=100, null=True, blank=True)
@@ -11,6 +12,7 @@ class Address(models.Model):
 
     def __str__(self):
         return self.address
+
 
 class Customer(models.Model):
     first_name = models.CharField(max_length=30)
@@ -22,11 +24,13 @@ class Customer(models.Model):
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
 
+
 class VehicleType(models.Model):
     name = models.CharField(max_length=50, unique=True)
 
     def __str__(self):
         return self.name
+
 
 class VehicleSize(models.Model):
     name = models.CharField(max_length=50, unique=True)
@@ -34,11 +38,13 @@ class VehicleSize(models.Model):
     def __str__(self):
         return self.name
 
+
 class Vehicle(models.Model):
     vehicle_type = models.ForeignKey(VehicleType, on_delete=models.CASCADE)
     date_created = models.DateField()
     real_cost = models.DecimalField(max_digits=10, decimal_places=2)
     size = models.ForeignKey(VehicleSize, on_delete=models.CASCADE)
+
 
 class Rental(models.Model):
     rental_date = models.DateField()
@@ -46,15 +52,18 @@ class Rental(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE)
 
+
 class RentalRate(models.Model):
     daily_rate = models.DecimalField(max_digits=5, decimal_places=2)
     vehicle_type = models.ForeignKey(VehicleType, on_delete=models.CASCADE)
     vehicle_size = models.ForeignKey(VehicleSize, on_delete=models.CASCADE)
 
+
 class RentalStation(models.Model):
     name = models.CharField(max_length=100)
     capacity = models.IntegerField()
     address = models.ForeignKey(Address, on_delete=models.CASCADE)
+
 
 class VehicleAtRentalStation(models.Model):
     arrival_date = models.DateField()
--- week6/day5/miniproject/bike_store/rent/views.py
+++ week6/day5/miniproject/bike_store/rent/views.py
@@ -2,13 +2,19 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .models import  Customer, Vehicle, Rental, RentalStation, VehicleAtRentalStation
-from .serializers import  CustomerSerializer, VehicleSerializer, RentalSerializers, RentalStationSerializers
+from .models import Customer, Vehicle, Rental, RentalStation, VehicleAtRentalStation
+from .serializers import (
+    CustomerSerializer,
+    VehicleSerializer,
+    RentalSerializers,
+    RentalStationSerializers,
+)
 from rest_framework.permissions import AllowAny
 
-      
+
 class RentalAPIView(APIView):
     permission_classes = [AllowAny]
+
     # Get with or without ID
     def get(self, request, **kwargs):
         id = kwargs.get("pk", None)
@@ -16,22 +22,22 @@ class RentalAPIView(APIView):
             try:
                 rental = Rental.objects.get(id=id)
                 serializer = RentalSerializers(rental)
+                return Response(serializer.data)
 
             except Rental.DoesNotExist:
                 return Response(status=status.HTTP_404_NOT_FOUND)
         else:
-            rentals = Rental.objects.order_by('return_date', 'rental_date')
+            rentals = Rental.objects.order_by("return_date", "rental_date")
             serializer = RentalSerializers(rentals, many=True)
             return Response(serializer.data)
 
-    #Post
-    def post (self, request, format=None):
+    # Post
+    def post(self, request, format=None):
         serializer = RentalSerializers(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
 
     def put(self, request, pk, *args, **kwargs):
         rental = Rental.objects.get(id=pk)
@@ -46,6 +52,7 @@ class RentalAPIView(APIView):
         rental.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
+
 class CustomerAPIView(APIView):
     permission_classes = [AllowAny]
 
@@ -59,17 +66,16 @@ class CustomerAPIView(APIView):
             except Customer.DoesNotExist:
                 return Response(status=status.HTTP_404_NOT_FOUND)
         else:
-            customers = Customer.objects.order_by('first_name')
+            customers = Customer.objects.order_by("first_name")
             serializer = CustomerSerializer(customers, many=True)
         return Response(serializer.data)
 
-    def post (self, request, format=None):
-            serializer = CustomerSerializer(data=request.data)
-            if serializer.is_valid():
-                serializer.save()
-                return Response(serializer.data, status=status.HTTP_201_CREATED)
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
+    def post(self, request, format=None):
+        serializer = CustomerSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 # GET specific vehicle or it will group by type
@@ -86,33 +92,32 @@ class VehicleAPIView(APIView):
             except Rental.DoesNotExist:
                 return Response(status=status.HTTP_404_NOT_FOUND)
         else:
-            vehicles = Vehicle.objects.order_by('vehicle_type')
+            vehicles = Vehicle.objects.order_by("vehicle_type")
             serializer = RentalSerializers(vehicles, many=True)
 
             # group vehicles by type
             grouped_vehicles = {}
             for vehicle in serializer.data:
-                type_id = vehicle['vehicle_type']['id']
+                type_id = vehicle["vehicle_type"]["id"]
                 if type_id not in grouped_vehicles:
                     grouped_vehicles[type_id] = {
-                        'vehicle_type': vehicle['vehicle_type'],
-                        'vehicles': [vehicle]
+                        "vehicle_type": vehicle["vehicle_type"],
+                        "vehicles": [vehicle],
                     }
                 else:
-                    grouped_vehicles[type_id]['vehicles'].append(vehicle)
-            
-            return Response(list(grouped_vehicles.values()))
-                     
+                    grouped_vehicles[type_id]["vehicles"].append(vehicle)
 
-# GET/POST /rent/vehicle/add: Allows for adding a new vehicle.
-    def post (self, request, format=None):
+            return Response(list(grouped_vehicles.values()))
+
+    # GET/POST /rent/vehicle/add: Allows for adding a new vehicle.
+    def post(self, request, format=None):
         serializer = VehicleSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-# PUT/DELETE /rent/vehicle/<pk>: Displays, updates, or deletes a specific vehicle.
+    # PUT/DELETE /rent/vehicle/<pk>: Displays, updates, or deletes a specific vehicle.
     def put(self, request, pk, *args, **kwargs):
         vehicle = Vehicle.objects.get(id=pk)
         serializer = VehicleSerializer(instance=vehicle, data=request.data)
@@ -126,7 +131,9 @@ class VehicleAPIView(APIView):
         vehicle.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
+
 # Get all the rental stations, and also filter by ID with the vehicles on it
+
 
 class RentalStationAPIView(APIView):
     def get(self, request, **kwargs):
@@ -134,56 +141,69 @@ class RentalStationAPIView(APIView):
         if id is not None:
             try:
                 rental_station = RentalStation.objects.get(id=id)
-                vehicles_at_station = VehicleAtRentalStation.objects.filter(station=rental_station)
-                
-                #GET distribution stats
+                vehicles_at_station = VehicleAtRentalStation.objects.filter(
+                    station=rental_station
+                )
+
+                # GET distribution stats
                 rental_station_data = RentalStationSerializers(rental_station).data
-                vehicles_data = VehicleAtRentalStation(vehicles_at_station, many =True).data
-                
-                return Response({"rental_station": rental_station_data,
-                                 "vehicles_at_station": vehicles_data})
+                vehicles_data = VehicleAtRentalStation(
+                    vehicles_at_station, many=True
+                ).data
+
+                return Response(
+                    {
+                        "rental_station": rental_station_data,
+                        "vehicles_at_station": vehicles_data,
+                    }
+                )
 
             except RentalStation.DoesNotExist:
                 return Response(status=status.HTTP_404_NOT_FOUND)
         else:
             rental_stations = Rental.objects.all()
             serializer = RentalSerializers(rental_stations, many=True)
-            return Response(serializer.data) 
+            return Response(serializer.data)
 
-# POST /rent/station/add: Allows for creating a new rental station.
-    def post (self, request, action):
-        if action == 'add':
+    # POST /rent/station/add: Allows for creating a new rental station.
+    def post(self, request, action):
+        if action == "add":
             serializer = RentalStationSerializers(data=request.data)
             if serializer.is_valid():
                 serializer.save()
                 return Response(serializer.data, status=status.HTTP_201_CREATED)
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        
-        elif action == 'distribute':
+
+        elif action == "distribute":
             # Distribute vehicles among stations
             try:
-                total_capacity = sum(station.capacity for station in RentalStation.objects.all())
+                total_capacity = sum(
+                    station.capacity for station in RentalStation.objects.all()
+                )
                 total_vehicles = VehicleAtRentalStation.objects.count()
 
                 if total_capacity >= total_vehicles:
                     # Calculate the average number of vehicles to distribute to each station
-                    avg_vehicles_per_station = total_vehicles // RentalStation.objects.count()
+                    avg_vehicles_per_station = (
+                        total_vehicles // RentalStation.objects.count()
+                    )
 
                     # Distribute vehicles evenly among stations
                     for station in RentalStation.objects.all():
-                        vehicles_to_distribute = VehicleAtRentalStation.objects.filter(station=None)[:avg_vehicles_per_station]
+                        vehicles_to_distribute = VehicleAtRentalStation.objects.filter(
+                            station=None
+                        )[:avg_vehicles_per_station]
                         for vehicle in vehicles_to_distribute:
                             vehicle.station = station
                             vehicle.save()
 
                     return Response({"message": "Vehicles distributed successfully"})
                 else:
-                    return Response({"error": "Not enough capacity to distribute vehicles"}, status =status.HTTP_400_BAD_REQUEST)
+                    return Response(
+                        {"error": "Not enough capacity to distribute vehicles"},
+                        status=status.HTTP_400_BAD_REQUEST,
+                    )
             except Exception as e:
                 return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-
-    
