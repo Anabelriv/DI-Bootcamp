3fa65cc0c192c2b4f7d33317b6e27b1c9a8e557a
anabelriv <anabelriverav@gmail.com>
2023-08-23 14:58:03 +0300

    dc

--- week7/day4/scritp.js
+++ week7/day4/scritp.js
@@ -100,20 +100,20 @@ const colorChoices = colors.map((color, index) => {
 // Instructions
 // Using this object:
 
-// const students = [{ name: "Ray", course: "Computer Science", isPassed: true },
-// { name: "Liam", course: "Computer Science", isPassed: false },
-// { name: "Jenner", course: "Information Technology", isPassed: true },
-// { name: "Marco", course: "Robotics", isPassed: true },
-// { name: "Kimberly", course: "Artificial Intelligence", isPassed: false },
-// { name: "Jamie", course: "Big Data", isPassed: false }];
+const students = [{ name: "Ray", course: "Computer Science", isPassed: true },
+{ name: "Liam", course: "Computer Science", isPassed: false },
+{ name: "Jenner", course: "Information Technology", isPassed: true },
+{ name: "Marco", course: "Robotics", isPassed: true },
+{ name: "Kimberly", course: "Artificial Intelligence", isPassed: false },
+{ name: "Jamie", course: "Big Data", isPassed: false }];
 // // Using the filter() method, create a new array, containing the students that passed the course.
 // // Bonus : Chain the filter method with a forEach method, to congratulate the students with their name and course name (ie. “Good job Jenner, you passed the course in Information Technology”, “Good Job Marco you passed the course in Robotics” ect…)
 
-// const passedStudents = students.filter(student => student.isPassed);
+const passedStudents = students.filter(student => student.isPassed);
 
-// passedStudents.forEach(student => {
-//     console.log(`Good job ${student.name}, you passed the course in ${student.course}`);
-// });
+passedStudents.forEach(student => {
+    console.log(`Good job ${student.name}, you passed the course in ${student.course}`);
+});
 
 
 
