7d1c22a6491cbc22d3863e36310789bcbcdbe4dd
anabelriv <anabelriverav@gmail.com>
2023-08-23 14:57:12 +0300

    dc

---
+++ week7/day4/dc/script.js
@@ -0,0 +1,59 @@
+// Using this array:
+
+const gameInfo = [
+    {
+        username: "john",
+        team: "red",
+        score: 5,
+        items: ["ball", "book", "pen"]
+    },
+    {
+        username: "becky",
+        team: "blue",
+        score: 10,
+        items: ["tape", "backpack", "pen"]
+    },
+    {
+        username: "susy",
+        team: "red",
+        score: 55,
+        items: ["ball", "eraser", "pen"]
+    },
+    {
+        username: "tyson",
+        team: "green",
+        score: 1,
+        items: ["book", "pen"]
+    },
+];
+// Create an array using forEach that contains all the usernames from the gameInfo array, add an exclamation point (ie. “!”) to the end of every username.
+// The new array should look like this :
+// const usernames = ["john!", "becky!", "susy!", "tyson!"]
+const allUsers = [];
+gameInfo.forEach(player => {
+    allUsers.push(player.username + "!");
+});
+
+console.log(allUsers)
+
+// 2. Create an array using forEach that contains the usernames of all players with a score bigger than 5.
+// The new array should look like this :
+
+// const winners = ["becky", "susy"]
+const playerBigggerFive = [];
+gameInfo.forEach(player => {
+    if (player.score > 5) {
+        winners.push(player.username);
+    }
+});
+
+console.log(winners);
+
+
+// 3. Find and display the total score of the users. (Hint: The total score is 71)
+
+const totalScore = gameInfo.reduce((accumulator, player) => {
+    return accumulator + player.score;
+}, 0);
+
+console.log("Total score:", totalScore);
--- week7/day4/scritp.js
+++ week7/day4/scritp.js
@@ -19,15 +19,15 @@
 // Instructions
 // Using these arrays :
 
-// const colors = ["Blue", "Green", "Red", "Orange", "Violet", "Indigo", "Yellow"];
-// const ordinal = ["th", "st", "nd", "rd"];
+const colors = ["Blue", "Green", "Red", "Orange", "Violet", "Indigo", "Yellow"];
+const ordinal = ["th", "st", "nd", "rd"];
 // // Write a JavaScript program that displays the colors in the following order : “1st choice is Blue .” “2nd choice is Green.” “3rd choice is Red.” ect…
 // // Hint : Use the array methods taught in class and ternary operator.
-// const colorChoices = colors.map((color, index) => {
-//     const suffixIndex = index === 0 ? 1 : index === 1 ? 2 : index === 2 ? 3 : 0;
-//     const ordinalSuffix = ordinal[suffixIndex];
-//     return `${index + 1}${ordinalSuffix} choice is ${color}.`;
-// });
+const colorChoices = colors.map((color, index) => {
+    const suffixIndex = index === 0 ? 1 : index === 1 ? 2 : index === 2 ? 3 : 0;
+    const ordinalSuffix = ordinal[suffixIndex];
+    return `${index + 1}${ordinalSuffix} choice is ${color}.`;
+});
 
 // console.log(colorChoices);
 
